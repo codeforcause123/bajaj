4eb2b30298a0e8869e92aeb2cc9274b3383ee3b5
Aayush <aayush.130702@gmail.com>
2023-05-23 13:12:22 +0530

    Updated

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,9 +1,9 @@
 import React from "react";
 const MyComponent = (props) => {
   return (
-    <div className="text-centre xt-card rounded-2xl bg-slate-900">
+    <div className="text-centre xt-card rounded-xl bg-slate-900 p-2">
       {props.data.map((item, index) => (
-        <div className="my-4 bg-yellow-600 rounded-2xl" key={index}>
+        <div className="my-4 bg-red-600 rounded-2xl  text-white h-20" key={index}>
           <p>{item.name}</p>
           <p>{item.designation}</p>
           <p>{item.skills}</p>
